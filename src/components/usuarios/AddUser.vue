6fa21fa5a4c366c70b611e813eb15dd82bab7cfd
marciock <marciockalves@gmail.com>
2020-08-01 07:31:33 -0300

    layout

---
+++ src/components/usuarios/AddUser.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container mt-5">
+        <h3>Novo Usuário </h3>
+    
+    <form >
+        <div class="col-sm-8">
+            <div class="form-group">
+                <label for="name">Nome</label>
+                <input type="text" id="name" class="form-control" >
+            </div>
+            <div class="form-group ">
+                <label for="lastname">Sobrenome</label>
+                <input type="text" id="lastname" class="form-control" >
+            </div>
+            <div class="form-group ">
+                <label for="email">Email</label>
+                <input type="mail" id="email" class="form-control" >
+            </div>
+            <div class="form-group ">
+                <label for="password">Senha</label>
+                <input type="password" id="password" class="form-control" >
+            </div>
+            <div class="form-group ">
+                <label for="confirm">Confirme a Senha</label>
+                <input type="password" id="confirme" class="form-control" v-model="confirm">
+            </div>
+            <div class="float-right " >
+               
+                <button class="btn btn-success" type="submit" v-if="modelo.password === confirm" >Salvar</button>
+                <button class="btn btn-success" type="submit" v-else disabled="true" >Salvar</button>
+                <router-link class="btn btn-danger ml-3"  to="/usuarios">Cancelar</router-link>
+            </div>
+           
+        </div>
+    </form>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'AddUser',
+    
+}
+</script>
---
+++ src/components/usuarios/TableView.vue
@@ -0,0 +1,43 @@
+<template>  
+    <div class="container">
+        <table class="table table-stripped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>Email</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr >
+                    <td></td>
+                   
+                </tr>
+            </tbody>
+        </table>
+        <ButtonUrl url="/adduser" btnClass="btn btn-success rounded-circle" fa="fa fa-plus" />
+    </div>
+    
+</template>
+<script>
+
+import ButtonUrl from '../buttons/ButtonUrl';
+export default {
+    name:'TableView',
+    components:{
+        ButtonUrl
+    },
+    data:()=>{
+        return{
+            registros:{},
+           
+        }
+    },
+    
+    
+
+      
+    
+}
+</script>
